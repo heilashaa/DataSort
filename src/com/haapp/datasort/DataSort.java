d235b58e662290d9c7335bb335f8a10e93b29140
Alexandr Heilash <alexandr.heilash@gmail.com>
2020-08-05 12:04:24 +0300

    fix

--- src/com/haapp/datasort/DataSort.java
+++ src/com/haapp/datasort/DataSort.java
@@ -3,16 +3,24 @@ package com.haapp.datasort;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DataSort {
 
     public static void main(String[] args) {
 
-        List<String> data01 = Arrays.asList("1", "3", "2", "18", "9");
-        List<String> data02 = Arrays.asList("17-01/15", "17-01/10", "24-16/32", "02-03/13", "79-04/18", "25-11/11");
-        List<String> data03 = Arrays.asList("23-Л", "4-П", "12-К", "3-Ж");
-        List<String> data04 = Arrays.asList("С-26", "К-5", "А-15");
-        List<String> data05 = Arrays.asList("55/АР", "10/Д", "100/ДК", "99/ДО");
+//        List<String> data01 = Arrays.asList("1", "3", "2", "18", "9");
+//        List<String> data02 = Arrays.asList("17-01/15", "17-01/10", "24-16/32", "02-03/13", "79-04/18", "25-11/11");
+//        List<String> data03 = Arrays.asList("23-Л", "4-П", "12-К", "3-Ж");
+//        List<String> data04 = Arrays.asList("С-26", "К-5", "А-15");
+//        List<String> data05 = Arrays.asList("55/АР", "10/Д", "100/ДК", "99/ДО");
+
+        List<String> data01 = Arrays.asList("1", "17-01/15", "17-01/10", "24-16/32", "02-03/13", "79-04/18", "25-11/11");
+        List<String> data02 = Arrays.asList("С", "К-5", "А-15");
+        List<String> data03 = Arrays.asList("С 26", "К-5", "А-15");
+        List<String> data04 = Arrays.asList("С-[26]", "К-5", "А-15");
+        List<String> data05 = Arrays.asList("55 /АР", "10/Д", "100/ДК", "99/ДО");
+        List<String> data06 = Arrays.asList(" 55/АР", "10/Д", "100/ДК", "99/ДО");
 
         System.out.println("Start: " + data01);
         System.out.println("Finish: " + sortedData(data01));
@@ -33,38 +41,60 @@ public class DataSort {
         System.out.println("Start: " + data05);
         System.out.println("Finish: " + sortedData(data05));
         System.out.println("________________________");
+
+        System.out.println("Start: " + data06);
+        System.out.println("Finish: " + sortedData(data06));
+        System.out.println("________________________");
     }
 
     public static List<String> sortedData(List<String> data) {
-        char splitLast = 0;
-        char splitFirst = 0;
-        int index = 0;
-        String reg = "[1-9]+[0-9]*";
-        for (String str : data) {
-            if (str.matches(reg)) {
-                data.sort(Comparator.comparingInt(Integer::parseInt));
-                return data;
-            }
-            int indexStart = 0;
-            splitFirst = 0;
-            for (int i = 0; i < str.length(); i++) {
-                if (str.substring(indexStart, i + 1).matches(reg)) {
-                    index = indexStart;
-                } else {
-                    splitLast = str.charAt(i);
-                    if (splitFirst == 0) {
-                        splitFirst = str.charAt(i);
+        List<String> trimData = data.stream().map(s -> s
+                .trim()                                     //если допустимы только начальные и конечные пробелы
+                .replaceAll("\\s+", "")    //если допустимы любые пробелы
+        ).collect(Collectors.toList());
+
+        try {
+            char splitLast = 0;
+            char splitFirst = 0;
+            int index = 0;
+            String reg = "[1-9]+[0-9]*";
+            for (String str : trimData) {
+                if (str.matches(reg)) {
+                    trimData.sort(Comparator.comparingInt(Integer::parseInt));
+                    return trimData;
+                }
+                int indexStart = 0;
+                splitFirst = 0;
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.substring(indexStart, i + 1).matches(reg)) {
+                        index = indexStart;
+                    } else {
+                        splitLast = str.charAt(i);
+                        if (splitFirst == 0) {
+                            splitFirst = str.charAt(i);
+                        }
+                        indexStart = i + 1;
                     }
-                    indexStart = i + 1;
                 }
             }
-        }
-        if (index > 0) {
-            String splLast = String.valueOf(splitLast);
-            data.sort(Comparator.comparingInt(s -> Integer.parseInt(s.substring(s.lastIndexOf(splLast) + 1))));
-        } else {
-            String splFirst = String.valueOf(splitFirst);
-            data.sort(Comparator.comparingInt(s -> Integer.parseInt(s.substring(0, s.lastIndexOf(splFirst)))));
+
+            for (int j = 0; j < data.size(); j++) {
+                if (data.get(j).length() != trimData.get(j).length()) {
+                    System.out.println("!Внимание! В элементе " + data.get(j) + " найдены и убраны лишние пробелы");
+                }
+            }
+
+            if (index > 0) {
+                String splLast = String.valueOf(splitLast);
+                trimData.sort(Comparator.comparingInt(s -> Integer.parseInt(s.substring(s.lastIndexOf(splLast) + 1))));
+            } else {
+                String splFirst = String.valueOf(splitFirst);
+                trimData.sort(Comparator.comparingInt(s -> Integer.parseInt(s.substring(0, s.lastIndexOf(splFirst)))));
+            }
+
+            return trimData;
+        } catch (NumberFormatException ex) {
+            System.out.println("Нет общего индекса для сортировки. Данные не отсортированы");
         }
         return data;
     }
